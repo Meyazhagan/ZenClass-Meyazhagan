7b612c5848d4fbbe2f653287ced71b82eab3fecc
Meyazhagan <meyazhagan.ofcl@gmail.com>
2021-07-23 10:51:34 +0530

    JSON

---
+++ Day5/JSON.js
@@ -0,0 +1,49 @@
+let technical_spec = {
+    dimensions:{
+        overAllLength:'3500 mm',
+        overAllWidth: '1600 mm',
+        overAllHeight: '1490 mm',
+        wheelBase: '2360 mm',
+        trackWidth: {
+            front:'1405 mm',
+            rear: '1400 mm'
+        },
+        minimumTurningRadius:'4.5 m',
+        minimumGroundClearence: '170 mm'
+    },
+    capacities:{
+        seatingCapacity: '5 persons',
+        fuelTankCapacity : '35 liters'
+    },
+    engine:{
+        type :'KB-Series',
+        numberOfCylinders : 3,
+        numberOfValues : 12,
+        capacity: '998 cc/cm3',
+        boreXStroke: '73.0 mm x 79.5 mm',
+        compressionRatio : '10:1',
+        maximumPower : '67/6,200 PS/rpm',
+        maximumTorque : '90/3,500 Nm/rpm',
+        fuelDistribution : 'Multipoint Injection'
+    },
+    transmission : {
+        type : '5-speed MT'
+    },
+    chassis :{
+        steering : 'Rack & Pinion, Power Assisted',
+        brakes : {
+            front : 'ventilated discs',
+            rear: 'Drums'
+        },
+        suspension : {
+            front : 'MacPherson strut & coil spring',
+            rear: 'Isolated trailing link & coil spring'
+        },
+        shockAbsorbers: 'Gas Filled',
+        tyre : '155/80R13'
+    },
+    weights:{
+        kerbWeight: '860-880 kg',
+        grossVehicleWeight : '1320 kg'
+    }
+}
