c60ef47b0bbe133f92d639595fdf4403c28e11d2
Meyazhagan <meyazhagan.ofcl@gmail.com>
2021-08-15 18:28:00 +0530

    day 11

--- Day5/task1.js
+++ Day5/task1.js
@@ -1,6 +1,6 @@
 // 1. Do the below programs in anonymous function & IIFE
 let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-//     a. Print odd numbers in an array
+console.log("a. Print odd numbers in an array");
 let findOdd = function (arr) {
   arr.forEach((element) => {
     if (element % 2 !== 0) {
@@ -9,13 +9,14 @@ let findOdd = function (arr) {
   });
 };
 findOdd(arr);
-//     b. Convert all the strings to title caps in a string array
+console.log("b. Convert all the strings to title caps in a string array");
 let str = "convert all the strings to title caps in a string array".split(" ");
 for (let i = 0; i < str.length; i++) {
   str[i] = str[i][0].toUpperCase() + str[i].slice(1);
 }
 console.log(str.join(" "));
-//     c. Sum of all numbers in an array
+
+console.log("c. Sum of all numbers in an array");
 (function (arr) {
   let sum = 0;
   arr.forEach((element) => {
@@ -23,11 +24,26 @@ console.log(str.join(" "));
   });
   console.log(sum);
 })(arr);
-//     d. Return all the prime numbers in an array
-
+console.log("d. Return all the prime numbers in an array");
+function primeNumber(max) {
+  var notPrimes = [],
+    i,
+    j,
+    primes = [];
+  for (i = 2; i <= max; ++i) {
+    if (!notPrimes[i]) {
+      primes.push(i);
+      for (j = i << 1; j <= max; j += i) {
+        notPrimes[j] = true;
+      }
+    }
+  }
+  return primes;
+}
+console.log(primeNumber(15).join(" "));
 //     e. Return all the palindromes in an array
 
-//     f. Return median of two sorted arrays of same size
+console.log("f. Return median of two sorted arrays of same size");
 let arr1 = [1, 2, 3, 4, 5];
 let arr2 = [6, 7, 8, 9, 10];
 findMedianSortedArrays = function (nums1, nums2) {
@@ -62,6 +78,28 @@ findMedianSortedArrays = function (nums1, nums2) {
 };
 let median = findMedianSortedArrays(arr1, arr2);
 console.log(median);
-//     g. Remove duplicates from an array
+console.log("g. Remove duplicates from an array");
 
-//     h. Rotate an array by k times
+let removeDuplicate = function (arr) {
+  let freq = {};
+  for (let value of arr) {
+    freq[value] = ++freq[value] || 1;
+    if (freq[value] > 1) {
+      de;
+    }
+    console.log(value);
+  }
+};
+removeDuplicate([1, 2, 2, 4, 5, 4]);
+console.log("h. Rotate an array by k times");
+
+let rotate = (arr, k) => {
+  while (k != 0) {
+    arr.push(arr.shift());
+    k--;
+  }
+};
+
+let array = [1, 2, 3, 4, 5, 6, 7];
+rotate(array, 2);
+console.log(array.join(" "));
